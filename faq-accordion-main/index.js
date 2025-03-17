8929a4a2a3fd38db1488cab90ec2dc20f2ff2cd8
kunal90803 <palk27530@gmail.com>
2025-03-17 14:55:54 +0530

    add accessibility

--- faq-accordion-main/index.js
+++ faq-accordion-main/index.js
@@ -1,15 +1,16 @@
 const para = document.querySelectorAll("p");
+const btn = document.querySelectorAll(".btn");
+const h3s = document.querySelectorAll(".ques");
 para.forEach((p) => {
   p.classList.add("hide");
 });
 
-const btn = document.querySelectorAll(".btn");
-const h3s = document.querySelectorAll(".ques");
 function toggleAnswer(button, targetPara) {
   const target = button.getAttribute("data-target");
 
   if (!targetPara.classList.contains("hide")) {
     targetPara.classList.add("hide");
+    button.setAttribute("aria-expanded", "false");
 
     button.setAttribute("src", "./assets/images/icon-plus.svg");
     return;
@@ -24,6 +25,7 @@ function toggleAnswer(button, targetPara) {
   });
 
   targetPara.classList.remove("hide");
+  button.setAttribute("aria-expanded", "true");
   button.setAttribute("src", "./assets/images/icon-minus.svg");
 }
 
