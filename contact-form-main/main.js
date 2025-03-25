8cf691981acc3683e8a0fbfafdf02da208711af8
kunal90803 <palk27530@gmail.com>
2025-03-26 02:06:49 +0530

    update contact form

--- contact-form-main/main.js
+++ contact-form-main/main.js
@@ -4,27 +4,64 @@ const firstName=document.querySelector("#firstname");
 const lastName=document.querySelector("#lastname");
 const emailID=document.querySelector("#emailId");
 const message=document.querySelector("#message");
+const checkbox=document.querySelector("#concent");
+const queryRadios = document.querySelectorAll('input[name="query"]');
+const queryError = document.querySelector('.query .error'); 
+const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 clickButton.addEventListener("click",(e)=>{
     e.preventDefault();
     let isValid = true;
     if (firstName.value.trim() === "") {
         // errors[0].innerHTML = "This field is required";
-        errors[0].style.display = "block"; 
+        errors[0].style.visibility = "visible";
         isValid = false;
     } 
     if (lastName.value.trim() === "") {
-        // errors[0].innerHTML = "This field is required";
-        errors[1].style.display = "block"; 
+        // errors[1].innerHTML = "This field is required";
+        errors[1].style.visibility = "visible";
         isValid = false;
     } 
-    if (emailId.value.trim() === "") {
-        // errors[0].innerHTML = "This field is required";
-        errors[2].style.display = "block"; 
+    if (emailId.value.trim()   === "" || !pattern.test(emailId.value)) {
+        // errors[2].innerHTML = "Please enter a valid email address";
+        errors[2].style.visibility = "visible";
         isValid = false;
     } 
     if (message.value.trim() === "") {
         // errors[0].innerHTML = "This field is required";
-        errors[3].style.display = "block"; 
+        errors[4].style.visibility = "visible";
         isValid = false;
     } 
+    if(!checkbox.checked){
+        errors[5].style.visibility = "visible";
+        isValid = false;
+        document.querySelector(".final").style.marginBottom="5px";
+    }
+
+    let isSelected = false;
+
+    // Loop through the radio buttons to check if any is selected
+    queryRadios.forEach((radio) => {
+        if (radio.checked) {
+            isSelected = true;
+        }
+    });
+
+    if (!isSelected) {
+        queryError.style.visibility = "visible"; // Show error message
+    } else {
+        queryError.style.visibility = "hidden"; // Hide error message
+    }
+
+    if(isValid==true && isSelected==true){
+        // alert("Form submitted");
+        showToast();
+    }
 })
+
+let toastBox=document.querySelector(".toastBox");
+function showToast(){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = 'Thanks for completing the form we will be in touch soon';
+    toastBox.appendChild(toast);
+}
